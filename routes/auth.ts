6f96fabd974011952e102f328f471ec49f9f75e9
Siesta1510 <93849920+Siesta1510@users.noreply.github.com>
2023-03-28 22:22:17 +0700

    feat(prisma): 🎸 create login API

--- routes/auth.ts
+++ routes/auth.ts
@@ -12,13 +12,14 @@ const prisma = new PrismaClient();
 
 //REGISTER
 router.post("/register", async (req: Request, res: Response) => {
-  const { name, email, password }: User = req.body;
+  const { name, email, password, Role }: User = req.body;
   try {
     const newUser = await prisma.user.create({
       data: {
         name: name,
         email: email,
         password: CryptoJS.AES.encrypt(password, "nino").toString(),
+        Role: Role,
       },
     });
     console.log(newUser);
@@ -29,24 +30,39 @@ router.post("/register", async (req: Request, res: Response) => {
 });
 
 //LOGIN
-router.post("login", async (req: Request, res: Response) => {
+router.post("/login", async (req: Request, res: Response) => {
   try {
-    const { email }: User = req.body;
-    const user = await prisma.user.findUnique({
+    const { email, password }: User = req.body;
+    const user: User | null = await prisma.user.findUnique({
       where: {
         email: email,
       },
     });
-    !user && res.status(401).json("Wrong email or password!");
+    !user && res.status(401).json("Wrong password!");
+    console.log(process.env.SECRET_KEY);
     const bytes = CryptoJS.AES.decrypt(
       user?.password as string,
       process.env.SECRET_KEY as string
     );
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-    originalPassword !== user?.password &&
+    originalPassword !== password &&
       res.status(401).json("Wrong email or password!");
 
-    // const accessToken = jwt.sign()
+    const accessToken = jwt.sign(
+      { id: user?.id, role: user?.Role },
+      process.env.SECRET_KEY as string,
+      {
+        expiresIn: "5d",
+      }
+    );
+    const info = {
+      id: user?.id,
+      name: user?.name,
+      email: user?.password,
+      Role: user?.Role,
+    };
+    console.log(info);
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
